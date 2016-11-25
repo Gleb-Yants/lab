f1ae336fe64a7ffbb2a150d63f6cc276be81e4d7
Gleb-Yants <gleb.yants@gmail.com>
2016-11-26 02:24:33 +0300

    tested

---
+++ H2Test/src/main/java/dao/UserDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.User;
+
+import java.util.List;
+
+/**
+ * Created by Gleb_Yants on 26.11.2016.
+ */
+public interface UserDao {
+    User findByName(String name);
+
+    List<User> findAll();
+}
--- H2Test/src/test/java/H2SimpleTest.java
+++ H2Test/src/test/java/H2SimpleTest.java
@@ -1,11 +1,53 @@
+import dao.UserDao;
+import dao.UserDaoImpl;
+import model.User;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 /**
  * Created by Gleb_Yants on 26.11.2016.
  */
 public class H2SimpleTest {
-    @Test
-    public void name() throws Exception {
+
+        private EmbeddedDatabase db;
+
+        UserDao userDao;
+
+        @Before
+        public void setUp() {
+            //db = new EmbeddedDatabaseBuilder().addDefaultScripts().build();
+            db = new EmbeddedDatabaseBuilder()
+                    .setType(EmbeddedDatabaseType.H2)
+                    .addScript("db.sql/create-db.sql")
+                    .addScript("db.sql/insert-data.sql")
+                    .build();
+        }
+
+        @Test
+        public void testFindByname() {
+            NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(db);
+            UserDaoImpl userDao = new UserDaoImpl();
+            userDao.setNamedParameterJdbcTemplate(template);
+
+            User user = userDao.findByName("epam");
+
+            Assert.assertNotNull(user);
+            Assert.assertEquals(1, user.getId().intValue());
+            Assert.assertEquals("epam", user.getName());
+            Assert.assertEquals("epam@gmail.com", user.getEmail());
+
+        }
+
+        @After
+        public void tearDown() {
+            db.shutdown();
+        }
 
     }
-}
+
